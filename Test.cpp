e4abeb67951a4b6c8fab57904ce3cfa2ac508dee
itzik <itzik3877@gmail.com>
2022-05-10 20:36:39 +0300

    start tests

--- Test.cpp
+++ Test.cpp
@@ -16,11 +16,9 @@ TEST_CASE("Good Test"){
       .add_sub("CTO", "VP_SW") // Now the VP Software is subordinate to the CTO
       .add_sub("COO", "VP_BI")
       .add_sub("CFO", "VP_S");
-
-
-
-
+    // cout<<"19"<<endl;
     SUBCASE("start with begin_level_order and end with end_level_order"){
+        // cout<<"21"<<endl;
         vector<string> vec;
         vec.push_back("CEO");
         vec.push_back("CTO");
@@ -30,7 +28,9 @@ TEST_CASE("Good Test"){
         vec.push_back("VP_S");
         vec.push_back("VP_BI");
         size_t i=0;
+        // cout<<"org "<<organization<<endl;
         for (auto it = organization.begin_level_order(); it != organization.end_level_order(); ++it){
+            // cout<<"32"<<endl;
             CHECK((*it) == vec.at(i));
             i++;
         }
@@ -65,6 +65,31 @@ TEST_CASE("Good Test"){
             CHECK((*it) == vec.at(i));
             i++;
         }
-
     }
+    SUBCASE("change the root"){
+        organization.add_root("root");
+        vector<string> vec;
+        vec.push_back("root");   
+        vec.push_back("CTO");     
+        vec.push_back("VP_SW");
+        vec.push_back("CFO");
+        vec.push_back("VP_S");
+        vec.push_back("COO");
+        vec.push_back("VP_BI");
+        size_t i=0;
+        for (auto it=organization.begin_preorder(); it!=organization.end_preorder(); ++it) {
+            CHECK((*it) == vec.at(i));
+            i++;
+        }
+    }
+}
+TEST_CASE("Bad Test"){
+    OrgChart organization;
+    CHECK_THROWS(organization.add_sub("CEO","add"));
+    organization.add_root("CEO");
+    CHECK_THROWS(organization.add_sub("CEo","C"));
+    organization.add_sub("CEO", "CTO");
+    CHECK_THROWS(organization.add_sub("cto","add"));
+    organization.add_root("root");
+    CHECK_THROWS(organization.add_sub("CEO","add"));   
 }
---
+++ sources/OrgChart.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <OrgChart.hpp>
+
+using namespace std;
+using namespace ariel;
+
+OrgChart::OrgChart(){
+
+}
+OrgChart OrgChart::add_root(string str){
+    return *this;
+}
+OrgChart OrgChart::add_sub(string str1, string str2){
+    return *this;
+}
+vector<string>::iterator OrgChart::begin_level_order(){
+    vector<string>::iterator vec;
+    return vec;
+}
+vector<string>::iterator OrgChart::end_level_order(){
+    vector<string>::iterator vec;
+    return vec;
+}
+
+vector<string>::iterator OrgChart::begin_reverse_order(){
+    vector<string>::iterator vec;
+    return vec;
+}
+vector<string>::iterator OrgChart::reverse_order(){
+    vector<string>::iterator vec;
+    return vec;
+}
+vector<string>::iterator OrgChart::begin_preorder(){
+    vector<string>::iterator vec;
+    return vec;
+}
+vector<string>::iterator OrgChart::end_preorder(){
+    vector<string>::iterator vec;
+    return vec;
+}
+
+// ostream& operator<<(ostream& os,const OrgChart &org){
+//     return os;
+// }
